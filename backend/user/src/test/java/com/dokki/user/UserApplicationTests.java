29c63490e8ae89809bf409f1aa4e2d72f443f569
cmzlsho@gmail.com <cmzlsho@gmail.com>
2023-04-19 16:48:46 +0900

    [BE] setting : user 프로젝트 생성

---
+++ backend/user/src/test/java/com/dokki/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dokki.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
