2a88624ea2d3da238cb516389971a5e7586285dd
김빛누리 <bnurik104@gmail.com>
2023-04-22 09:13:47 +0900

    [BE] feat : 리뷰 서버 엔티티 추가

--- backend/review/src/main/java/com/dokki/review/entity/CommentEntity.java
+++ backend/review/src/main/java/com/dokki/review/entity/CommentEntity.java
@@ -1,5 +1,32 @@
 package com.dokki.review.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "comment")
 public class CommentEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
+	private Long userId;
+	@Column(nullable = false, length = 20)
+	private String bookId;
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String content;
+	@Column(nullable = false)
+	private Float score;
+
 }
