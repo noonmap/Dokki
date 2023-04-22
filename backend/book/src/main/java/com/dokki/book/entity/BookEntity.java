ed2522e645d09d360535d331e3e7695a00a6d1ee
김빛누리 <bnurik104@gmail.com>
2023-04-22 11:32:37 +0900

    [BE] feat : 책 서버 엔티티 추가
    - jpa physical-strategy 수정

--- backend/book/src/main/java/com/dokki/book/entity/BookEntity.java
+++ backend/book/src/main/java/com/dokki/book/entity/BookEntity.java
@@ -1,5 +1,34 @@
 package com.dokki.book.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "book")
 public class BookEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false, length = 200)
+	private String title;
+	@Column(length = 500)
+	private String coverImagePath;
+	@Column(columnDefinition = "TEXT")
+	private String summary;
+	@Column(nullable = false, length = 100)
+	private String author;
+	@Column(nullable = false, columnDefinition="DATE")
+	private LocalDate publishDate;
+	@Column(nullable = false)
+	private Integer totalPageCount;
 }
---
+++ backend/book/src/main/java/com/dokki/book/entity/BookStatisticsEntity.java
@@ -0,0 +1,54 @@
+package com.dokki.book.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "book_statistics")
+public class BookStatisticsEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 20)
+	private String bookId;
+
+	@Column(nullable = false)
+	private Integer completedUsers;
+
+	@Column(nullable = false)
+	private Integer readingUsers;
+
+	@Column(nullable = false)
+	private Integer bookmarkedUsers;
+
+	@Column(nullable = false)
+	private Float meanScore;
+
+	@Column(nullable = false)
+	private Integer meanReadTime;
+
+	@CreatedDate
+	@Column(columnDefinition="DATE", updatable = false, nullable = false)
+	@Convert(converter = Jsr310JpaConverters.LocalDateTimeConverter.class)
+	private LocalDate created;
+
+	@LastModifiedDate
+	@Column(columnDefinition="DATE", nullable = false)
+	@Convert(converter = Jsr310JpaConverters.LocalDateTimeConverter.class)
+	private LocalDate updated;
+
+}
