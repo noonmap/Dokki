50c0bc6ba79360b670fb583d58061da236c33186
김빛누리 <bnurik104@gmail.com>
2023-04-24 08:08:38 +0900

    [BE] feat : book 목업코드 추가

--- backend/book/src/main/java/com/dokki/book/controller/BookController.java
+++ backend/book/src/main/java/com/dokki/book/controller/BookController.java
@@ -1,5 +1,34 @@
 package com.dokki.book.controller;
 
 
+import com.dokki.book.dto.response.BookDetailResponseDto;
+import com.dokki.book.service.BookService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Log4j2
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/books")
+@Api(tags = "책 API")
 public class BookController {
+
+	public BookService bookService;
+
+
+	@GetMapping("/{bookId}")
+	@ApiOperation(value = "도서 상세 조회")
+	public ResponseEntity<BookDetailResponseDto> getBook(@PathVariable String bookId) {
+		BookDetailResponseDto bookDetail = bookService.getBook(bookId);
+		return ResponseEntity.ok(bookDetail);
+	}
+
 }
--- backend/book/src/main/java/com/dokki/book/service/BookService.java
+++ backend/book/src/main/java/com/dokki/book/service/BookService.java
@@ -1,5 +1,25 @@
 package com.dokki.book.service;
 
 
+import com.dokki.book.dto.response.BookDetailResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
 public class BookService {
+
+	/**
+	 * 책 상세정보 조회
+	 *
+	 * @param bookId 책 id
+	 * @return 책 정보 + 리뷰 정보
+	 */
+	public BookDetailResponseDto getBook(String bookId) {
+		return null;
+	}
+
 }
