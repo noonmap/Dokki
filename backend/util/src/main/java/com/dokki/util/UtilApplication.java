47c58b6873d857888548d303b73d6eeed3d2b6c0
JCW <codnjs8221@naver.com>
2023-04-25 15:39:06 +0900

    [BE] feat : util 프로젝트 생성

---
+++ backend/util/src/main/java/com/dokki/util/UtilApplication.java
@@ -0,0 +1,15 @@
+package com.dokki.util;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class UtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilApplication.class, args);
+	}
+
+}
