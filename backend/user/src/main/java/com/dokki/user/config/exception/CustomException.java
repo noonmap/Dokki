a8c42c9c609d4a031675f8a24d88cc704fd8475b
김빛누리 <cmzlsho@gmail.com>
2023-04-27 09:17:08 +0900

    [BE] feat : 공통 에러 핸들러 추가

---
+++ backend/user/src/main/java/com/dokki/user/config/exception/CustomException.java
@@ -0,0 +1,15 @@
+package com.dokki.user.config.exception;
+
+
+import com.dokki.util.common.error.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+@Getter
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException {
+
+	private final ErrorCode errorCode;
+
+}
