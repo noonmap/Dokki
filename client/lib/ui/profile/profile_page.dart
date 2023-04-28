ad101fdfd8e51b6eb339c0ff5af004fd20c95b53
김수민 <liijj@naver.com>
2023-04-28 17:37:06 +0900

    [FE] feat : 프로필 bio 구현중

--- client/lib/ui/profile/profile_page.dart
+++ client/lib/ui/profile/profile_page.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({Key? key}) : super(key: key);
+  const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("profile"));
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Icon(
+                  Icons.abc,
+                  size: 56,
+                ),
+                SizedBox(width: 24),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('닉네임이다'),
+                    Row(
+                      children: [
+                        Text('팔로잉  0'),
+                        SizedBox(width: 24),
+                        Text('팔로워  0'),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
