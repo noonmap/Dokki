9c5ae8bb73c5a5978ad1a8a8b8bd0bfaf071c61c
cmzlsho@gmail.com <cmzlsho@gmail.com>
2023-04-28 12:10:15 +0900

    [BE] feat : 책 찜하기 관련 api
    - 책 찜하기 리스트 / 추가 / 삭제
    
    S08P31E204-168,S08P31E204-169,S08P31E204-170

--- backend/book/src/main/java/com/dokki/book/service/BookmarkService.java
+++ backend/book/src/main/java/com/dokki/book/service/BookmarkService.java
@@ -1,14 +1,18 @@
 package com.dokki.book.service;
 
 
+import com.dokki.book.config.exception.CustomException;
 import com.dokki.book.entity.BookEntity;
+import com.dokki.book.entity.BookMarkEntity;
 import com.dokki.book.repository.BookRepository;
 import com.dokki.book.repository.BookmarkRepository;
+import com.dokki.util.common.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -25,11 +29,11 @@ public class BookmarkService {
 	 * 찜한 책 조회
 	 *
 	 * @param userId   유저 id
-	 * @param pageable
+	 * @param pageable 페이징
 	 * @return 유저가 찜한 책 페이지 객체
 	 */
-	public Page<BookEntity> getBookmarkList(Long userId, @RequestParam Pageable pageable) {
-		return null;
+	public Slice<BookMarkEntity> getBookmarkList(Long userId, @RequestParam Pageable pageable) {
+		return bookmarkRepository.findSliceByUserId(userId, pageable);
 	}
 
 
@@ -38,7 +42,17 @@ public class BookmarkService {
 	 *
 	 * @param bookId 책 id
 	 */
-	public void createBookmark(String bookId) {
+	public void createBookmark(Long userId, String bookId) {
+		isBookExist(bookId);
+		BookEntity bookEntity = bookRepository.getReferenceById(bookId);
+
+		// check bookmark already exist
+		boolean isBookmarkExist = bookmarkRepository.existsByUserIdAndBookId(userId, bookEntity);
+		if (isBookmarkExist) {
+			throw new CustomException(ErrorCode.INVALID_REQUEST);
+		}
+
+		bookmarkRepository.save(new BookMarkEntity(null, userId, bookEntity));
 	}
 
 
@@ -47,7 +61,24 @@ public class BookmarkService {
 	 *
 	 * @param bookId 책 id
 	 */
-	public void deleteBookmark(String bookId) {
+	@Transactional
+	public void deleteBookmark(Long userId, String bookId) {
+		isBookExist(bookId);
+		BookEntity bookEntity = bookRepository.getReferenceById(bookId);
+		bookmarkRepository.deleteByUserIdAndBookId(userId, bookEntity);
+	}
+
+
+	/**
+	 * 책 존재여부 확인, 존재하지 않다면 exception
+	 * 서비스 내부에서 사용
+	 */
+	private void isBookExist(String bookId) {
+		// check book exist, if not -> error
+		boolean isBookExist = bookRepository.existsById(bookId);
+		if (!isBookExist) {
+			throw new CustomException(ErrorCode.NOTFOUND_RESOURCE);
+		}
 	}
 
 }
