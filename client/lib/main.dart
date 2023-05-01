4ad20cdc732f7cd1506a7ce0fcb578361e5e8d3a
tjrghks96 <tjrghks96@naver.com>
2023-05-01 13:55:19 +0900

    [FE] feat : book deatil page

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -3,12 +3,15 @@ import 'package:dokki/providers/book_provider.dart';
 import 'package:dokki/providers/user_provider.dart';
 import 'package:dokki/utils/routes/routes.dart';
 import 'package:dokki/utils/routes/routes_name.dart';
+import "package:flutter/foundation.dart" as foundation;
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
-  await dotenv.load(fileName: "assets/config/.env");
+  foundation.defaultTargetPlatform == foundation.TargetPlatform.android
+      ? await dotenv.load(fileName: "assets/config/android/.env")
+      : await dotenv.load(fileName: "assets/config/ios/.env");
   runApp(const MyApp());
 }
 
@@ -27,8 +30,15 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
+        builder: (context, widget) {
+          return MediaQuery(
+            data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+            child: widget!,
+          );
+        },
         theme: ThemeData(
           scaffoldBackgroundColor: grayColor000,
+          fontFamily: 'Koddi',
         ),
         debugShowCheckedModeBanner: false,
         initialRoute: RoutesName.splash,
