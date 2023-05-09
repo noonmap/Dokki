66ba10c9ae33a761f60eaedaa36ecf80ea7dcb85
JCW <codnjs8221@naver.com>
2023-05-09 15:43:18 +0900

    [BE] feat : image file path 관련 util 함수 구현 중
    #S08P31E204-247

---
+++ backend/util/src/main/java/com/dokki/util/common/utils/FileUtils.java
@@ -0,0 +1,20 @@
+package com.dokki.util.common.utils;
+
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class FileUtils {
+
+	//	@Value("${file.upload.uri}")
+	//	public static final HOST_URI;
+
+	//	static public String getAbsoluteFilePath(String filePath) {
+	//		if (filePath.startsWith("http")) {
+	//			return filePath;
+	//		}
+	//
+	//	}
+
+}
