ed0769047a0bffe9ab8454db594a42e223a2ff56
김수민 <liijj@naver.com>
2023-05-09 17:49:48 +0900

    [FE] feat : 감정 일기 생성 페이지 구현중

--- client/lib/view/diary/diary_create_page.dart
+++ client/lib/view/diary/diary_create_page.dart
@@ -1,4 +1,6 @@
+import 'package:dokki/common/constant/colors.dart';
 import 'package:dokki/common/widget/opacity_loading.dart';
+import 'package:dokki/common/widget/paragraph.dart';
 import 'package:dokki/providers/book_provider.dart';
 import 'package:dokki/providers/diary_provider.dart';
 import 'package:flutter/material.dart';
@@ -34,10 +36,90 @@ class _DiaryCreatePageState extends State<DiaryCreatePage> {
     return Scaffold(
       body: dp.isCountLoading || bp.isDetailLoading
           ? const OpacityLoading()
-          : const Padding(
-              padding: EdgeInsets.fromLTRB(28, 40, 28, 40),
+          : Padding(
+              padding: const EdgeInsets.fromLTRB(28, 40, 28, 40),
               child: Column(
-                children: [Text('///')],
+                children: [
+                  // 상단 메뉴바
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: const SizedBox(
+                          width: 32,
+                          height: 32,
+                          child: Icon(
+                            Icons.arrow_back_ios,
+                            size: 22,
+                            color: brandColor300,
+                          ),
+                        ),
+                      ),
+                      const Paragraph(
+                        text: '감정 일기 생성',
+                        size: 18,
+                        weightType: WeightType.medium,
+                      ),
+                      const SizedBox(
+                        width: 32,
+                        height: 32,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 40),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.network(
+                        bp.book!.bookCoverPath,
+                        height: 180,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(width: 24),
+                      Flexible(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Paragraph(
+                              text: bp.book!.bookTitle,
+                              size: 24,
+                              weightType: WeightType.semiBold,
+                            ),
+                            const SizedBox(height: 12),
+                            Paragraph(
+                              text: bp.book!.bookAuthor,
+                              color: grayColor300,
+                            ),
+                            const SizedBox(height: 4),
+                            Paragraph(
+                              text: bp.book!.bookPublishYear,
+                              color: grayColor300,
+                            ),
+                            const SizedBox(height: 4),
+                            Paragraph(
+                              text: bp.book!.bookPublisher,
+                              color: grayColor300,
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 36),
+                  const Row(
+                    children: [
+                      Paragraph(
+                        text: '나의 감정 일기',
+                        size: 24,
+                        weightType: WeightType.semiBold,
+                      ),
+                    ],
+                  ),
+                  const TextField()
+                ],
               ),
             ),
     );
