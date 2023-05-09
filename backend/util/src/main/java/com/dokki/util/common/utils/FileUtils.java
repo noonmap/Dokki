3d61b23afa4c0fa49ee29945023bc136770504e9
JCW <codnjs8221@naver.com>
2023-05-09 17:42:32 +0900

    [BE] feat : test중

--- backend/util/src/main/java/com/dokki/util/common/utils/FileUtils.java
+++ backend/util/src/main/java/com/dokki/util/common/utils/FileUtils.java
@@ -1,6 +1,7 @@
 package com.dokki.util.common.utils;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -8,20 +9,36 @@ import java.io.File;
 
 
 @Component
+@Slf4j
+//@ConfigurationProperties(prefix = "file")
 public class FileUtils {
 
-	@Value("${file.upload.uri}")
-	private static String hostUri;
+	public static String hostUri;
+
+	//	public FileUtils(@Value("${file.upload.uri}") String uri) {
+	//		this.hostUri = uri;
+	//	}
 
 
-	static public String getAbsoluteFilePath(String filePath) {
+	public static String getAbsoluteFilePath(String filePath) {
+
 		if (filePath.startsWith("http")) {
 			return filePath;
 		}
 		if (filePath.startsWith("/resources")) { // resources 없애줌
 			filePath = filePath.replaceFirst("/resources", "");
 		}
-		return hostUri + File.pathSeparator + filePath;
+		if (filePath.startsWith("/"))
+			return hostUri + filePath;
+		return hostUri + File.separator + filePath;
+	}
+
+
+	@Value("${file.upload.uri}")
+	public void setHostUri(String uri) {
+		log.info("[HostUri] {}", uri);
+		log.info("[HostUri] {}", hostUri);
+		this.hostUri = uri;
 	}
 
 }
