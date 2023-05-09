46f46bf91d009099261fbd739b25e7430ad86df7
JCW <codnjs8221@naver.com>
2023-05-09 13:35:35 +0900

    [BE] fix : 개발 덜 된 코드 주석

--- backend/gateway/src/main/java/com/dokki/gateway/config/ExceptionConfig.java
+++ backend/gateway/src/main/java/com/dokki/gateway/config/ExceptionConfig.java
@@ -4,18 +4,16 @@ package com.dokki.gateway.config;
 import com.dokki.gateway.config.exception.GlobalFilterExceptionHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 
-@Configuration
+//@Configuration
 @RequiredArgsConstructor
 public class ExceptionConfig {
 
 	private final ObjectMapper objectMapper;
 
 
-	@Bean
+	//	@Bean
 	public GlobalFilterExceptionHandler globalFilterExceptionHandler() {
 		return new GlobalFilterExceptionHandler(objectMapper);
 	}
--- backend/gateway/src/main/java/com/dokki/gateway/config/exception/GlobalFilterExceptionHandler.java
+++ backend/gateway/src/main/java/com/dokki/gateway/config/exception/GlobalFilterExceptionHandler.java
@@ -4,17 +4,15 @@ package com.dokki.gateway.config.exception;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 
-@Component
+//@Component
 @RequiredArgsConstructor
-@Order(-1)
+//@Order(-1)
 public class GlobalFilterExceptionHandler implements ErrorWebExceptionHandler {
 
 	private final ObjectMapper objectMapper;
