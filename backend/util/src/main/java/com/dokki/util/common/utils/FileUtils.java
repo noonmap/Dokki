274902ed1bd1cfd5e3be10222905ce9fa6b86b61
JCW <codnjs8221@naver.com>
2023-05-09 17:42:32 +0900

    [BE] feat : test중

--- backend/util/src/main/java/com/dokki/util/common/utils/FileUtils.java
+++ backend/util/src/main/java/com/dokki/util/common/utils/FileUtils.java
@@ -2,26 +2,39 @@ package com.dokki.util.common.utils;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 
 
 @Component
+@ConfigurationProperties(prefix = "file")
 public class FileUtils {
 
-	@Value("${file.upload.uri}")
-	private static String hostUri;
+	public static String hostUri;
 
 
-	static public String getAbsoluteFilePath(String filePath) {
+	public FileUtils(@Value("${file.upload.uri}") String uri) {
+		this.hostUri = uri;
+	}
+
+
+	public static String getAbsoluteFilePath(String filePath) {
 		if (filePath.startsWith("http")) {
 			return filePath;
 		}
 		if (filePath.startsWith("/resources")) { // resources 없애줌
 			filePath = filePath.replaceFirst("/resources", "");
 		}
-		return hostUri + File.pathSeparator + filePath;
+		if (filePath.startsWith("/"))
+			return hostUri + filePath;
+		return hostUri + File.separator + filePath;
 	}
+
+	//	@Value("${file.upload.uri}")
+	//	public void setHostUri(String uri) {
+	//		this.hostUri = uri;
+	//	}
 
 }
