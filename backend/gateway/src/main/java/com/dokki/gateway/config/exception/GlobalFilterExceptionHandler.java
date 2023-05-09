c8005b4bf59a5632678a12f6d9e7db821cef5a55
JCW <codnjs8221@naver.com>
2023-05-09 16:29:38 +0900

    [BE] feat : Gateway Filter Exception Handler 추가
    #S08P31E204-243 #done

--- backend/gateway/src/main/java/com/dokki/gateway/config/exception/GlobalFilterExceptionHandler.java
+++ backend/gateway/src/main/java/com/dokki/gateway/config/exception/GlobalFilterExceptionHandler.java
@@ -1,18 +1,30 @@
 package com.dokki.gateway.config.exception;
 
 
+import com.dokki.util.common.error.ErrorCode;
+import com.dokki.util.common.error.ErrorResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.nio.charset.StandardCharsets;
 
-//@Component
+
+@Component
 @RequiredArgsConstructor
-//@Order(-1)
+@Order(-1)
+@Slf4j
 public class GlobalFilterExceptionHandler implements ErrorWebExceptionHandler {
 
 	private final ObjectMapper objectMapper;
@@ -27,7 +39,27 @@ public class GlobalFilterExceptionHandler implements ErrorWebExceptionHandler {
 		}
 
 		response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
-		return null;
+		CustomException customEx;
+
+		if (ex instanceof CustomException) { // Custom Exception이 throw 된 경우
+			customEx = (CustomException) ex;
+		} else { // Custom Exception이 throw 되지 않은 경우
+			customEx = new CustomException(ErrorCode.UNKNOWN_GATEWAY_ERROR);
+		}
+		response.setStatusCode(HttpStatus.valueOf(customEx.getStatus()));
+		String error = "Gateway Error";
+		try {
+			// ErrorCode를 다른 ExceptionHandler처럼 가공
+			//			error = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(new ErrorResponse(customEx.getErrorCode()));
+			error = objectMapper.writeValueAsString(new ErrorResponse(customEx.getErrorCode()));
+			log.error("[GlobalFilterExceptionHandler] {}", error);
+		} catch (JsonProcessingException e) {
+			log.error("JsonProcessingException : " + e.getMessage());
+		}
+
+		byte[] bytes = error.getBytes(StandardCharsets.UTF_8);
+		DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(bytes);
+		return exchange.getResponse().writeWith(Flux.just(buffer));
 	}
 
 }
