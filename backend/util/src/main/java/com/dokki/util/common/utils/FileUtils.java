ff74f246cea62cb62b703f3a2da3bb325e95ae62
JCW <codnjs8221@naver.com>
2023-05-09 17:15:24 +0900

    [BE] feat : FileUtil 구현 추가
    #S08P31E204-247 #done

--- backend/util/src/main/java/com/dokki/util/common/utils/FileUtils.java
+++ backend/util/src/main/java/com/dokki/util/common/utils/FileUtils.java
@@ -1,20 +1,27 @@
 package com.dokki.util.common.utils;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
 
 
 @Component
 public class FileUtils {
 
-	//	@Value("${file.upload.uri}")
-	//	public static final HOST_URI;
+	@Value("${file.upload.uri}")
+	private static String hostUri;
 
-	//	static public String getAbsoluteFilePath(String filePath) {
-	//		if (filePath.startsWith("http")) {
-	//			return filePath;
-	//		}
-	//
-	//	}
+
+	static public String getAbsoluteFilePath(String filePath) {
+		if (filePath.startsWith("http")) {
+			return filePath;
+		}
+		if (filePath.startsWith("/resources")) { // resources 없애줌
+			filePath = filePath.replaceFirst("/resources", "");
+		}
+		return hostUri + File.pathSeparator + filePath;
+	}
 
 }
