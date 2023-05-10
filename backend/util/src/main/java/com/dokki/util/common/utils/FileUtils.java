1f6d8b55088ffaaa7a9467728ca7234dc1296918
JCW <codnjs8221@naver.com>
2023-05-10 10:18:11 +0900

    [BE] feat : 임시 fileUtils 테스트
    - 공통 Config 파일을 만들어야할 필요성을 느낌

--- backend/util/src/main/java/com/dokki/util/common/utils/FileUtils.java
+++ backend/util/src/main/java/com/dokki/util/common/utils/FileUtils.java
@@ -2,7 +2,6 @@ package com.dokki.util.common.utils;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -10,17 +9,34 @@ import java.io.File;
 
 @Component
 @Slf4j
-//@ConfigurationProperties(prefix = "file")
 public class FileUtils {
 
-	public static String hostUri;
-
-	//	public FileUtils(@Value("${file.upload.uri}") String uri) {
-	//		this.hostUri = uri;
+	public static final String hostUri = "https://dokki.kr";
+	// FIXME: Property는 호출하는 서비스 쪽과 같은 패키지 내에 있는 파일 (application.yml)만 참고함.
+	//	static {
+	//		log.info("[static] get props");
+	//		Properties props = new Properties();
+	//		try {
+	//			props.load(FileUtils.class.getResourceAsStream("/application.yml"));
+	//			hostUri = props.getProperty("file.upload.uri");
+	//		} catch (IOException e) {
+	//			e.printStackTrace();
+	//		}
 	//	}
 
 
 	public static String getAbsoluteFilePath(String filePath) {
+		//		if (hostUri == null) {
+		//			log.info("[getAbsoluteFilePath] hostUri is null");
+		//			Properties props = new Properties();
+		//			try {
+		//				props.load(FileUtils.class.getResourceAsStream("/application.yml"));
+		//				hostUri = props.getProperty("file.upload.uri");
+		//			} catch (IOException e) {
+		//				e.printStackTrace();
+		//			}
+		//		}
+		//		log.info("[getAbsoluteFilePath] {}", hostUri);
 
 		if (filePath.startsWith("http")) {
 			return filePath;
@@ -31,14 +47,6 @@ public class FileUtils {
 		if (filePath.startsWith("/"))
 			return hostUri + filePath;
 		return hostUri + File.separator + filePath;
-	}
-
-
-	@Value("${file.upload.uri}")
-	public void setHostUri(String uri) {
-		log.info("[HostUri] {}", uri);
-		log.info("[HostUri] {}", hostUri);
-		this.hostUri = uri;
 	}
 
 }
