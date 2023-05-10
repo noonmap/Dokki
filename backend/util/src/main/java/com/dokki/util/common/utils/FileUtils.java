a315435f4457e46b1c6b1cfce1df35060e15f770
JCW <codnjs8221@naver.com>
2023-05-10 10:22:52 +0900

    [BE] feat : 임시 FileUtil 함수 구현
    #S08P31E204-247 #done

--- backend/util/src/main/java/com/dokki/util/common/utils/FileUtils.java
+++ backend/util/src/main/java/com/dokki/util/common/utils/FileUtils.java
@@ -1,7 +1,6 @@
 package com.dokki.util.common.utils;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -10,8 +9,9 @@ import java.io.File;
 @Component
 public class FileUtils {
 
-	@Value("${file.upload.uri}")
-	private static String hostUri;
+	// FIXME: Property는 호출하는 서비스 쪽과 같은 패키지 내에 있는 파일 (application.yml)만 참고함.
+	//	@Value("${file.upload.uri}")
+	private static final String hostUri = "https://dokki.kr";
 
 
 	static public String getAbsoluteFilePath(String filePath) {
@@ -20,6 +20,9 @@ public class FileUtils {
 		}
 		if (filePath.startsWith("/resources")) { // resources 없애줌
 			filePath = filePath.replaceFirst("/resources", "");
+		}
+		if (filePath.startsWith("/")) {
+			return hostUri + filePath;
 		}
 		return hostUri + File.pathSeparator + filePath;
 	}
