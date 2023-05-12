1d932bdf4cc0f8177bfa322111157bf8de64e029
JCW <codnjs8221@naver.com>
2023-05-12 14:31:24 +0900

    [BE] feat : 타임존 설정

--- backend/review/src/main/java/com/dokki/review/ReviewApplication.java
+++ backend/review/src/main/java/com/dokki/review/ReviewApplication.java
@@ -7,6 +7,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 
 @SpringBootApplication
 @EnableFeignClients
@@ -16,6 +19,12 @@ public class ReviewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewApplication.class, args);
+	}
+
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
