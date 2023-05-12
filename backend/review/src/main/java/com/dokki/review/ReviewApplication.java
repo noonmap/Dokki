ba7868aa810bfddcad025fba63644cf2929887a3
JCW <codnjs8221@naver.com>
2023-05-12 14:54:02 +0900

    [BE] fix : timezone check 로그 출력

--- backend/review/src/main/java/com/dokki/review/ReviewApplication.java
+++ backend/review/src/main/java/com/dokki/review/ReviewApplication.java
@@ -1,6 +1,7 @@
 package com.dokki.review;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDate;
 import java.util.TimeZone;
 
 
@@ -15,6 +17,7 @@ import java.util.TimeZone;
 @EnableFeignClients
 @EnableDiscoveryClient
 @EnableJpaAuditing // created, updated 관련
+@Slf4j
 public class ReviewApplication {
 
 	public static void main(String[] args) {
@@ -25,6 +28,7 @@ public class ReviewApplication {
 	@PostConstruct
 	void started() {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		log.info("Current Time : {}", LocalDate.now());
 	}
 
 }
