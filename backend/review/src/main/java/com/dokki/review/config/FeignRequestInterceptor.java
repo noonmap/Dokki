0f6f33f15bd07f28c68c1aecf427d9ffc221d134
JCW <codnjs8221@naver.com>
2023-05-12 10:14:27 +0900

    [BE] fix : feign client에 authorization header 추가

--- backend/review/src/main/java/com/dokki/review/config/FeignRequestInterceptor.java
+++ backend/review/src/main/java/com/dokki/review/config/FeignRequestInterceptor.java
@@ -3,6 +3,7 @@ package com.dokki.review.config;
 
 import com.dokki.util.common.utils.SessionUtils;
 import feign.RequestInterceptor;
+import org.apache.http.HttpHeaders;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,7 @@ public class FeignRequestInterceptor {
 			requestTemplate.header("USER_ID", String.valueOf(SessionUtils.getUserId()));
 			requestTemplate.header("USER_NICKNAME", SessionUtils.getUserNickname());
 			requestTemplate.header("USER_PROFILE_IMAGE_PATH", SessionUtils.getUserProfileImagePath());
+			requestTemplate.header(HttpHeaders.AUTHORIZATION, HttpHeaders.AUTHORIZATION);
 		};
 	}
 
