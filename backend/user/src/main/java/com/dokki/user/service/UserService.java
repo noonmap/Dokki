930fce0ad34f8a16af7e609fae1755a21672df4e
leeflection <leeflection@gmail.com>
2023-05-16 11:58:40 +0900

    프로필 사진 변경 시 성공 여부 -> url 반환

--- backend/user/src/main/java/com/dokki/user/service/UserService.java
+++ backend/user/src/main/java/com/dokki/user/service/UserService.java
@@ -138,8 +138,7 @@ public class UserService {
 			uploadFile.transferTo(path.toFile());
 
 			/** 현재 사용자 정보를 가져와서 id로 조회를 한다.**/
-			//Long id = Long.valueOf(SecurityUtil.getCurrentId().get());
-			Long id = 121L;
+			Long id = Long.valueOf(SecurityUtil.getCurrentId().get());
 			Optional<UserEntity> user = userRepository.findById(id);
 			user.get().setProfileImagePath(uploadFolder + "/" + savingFileName);
 			userRepository.save(user.get());
