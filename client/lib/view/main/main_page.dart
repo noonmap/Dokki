859bc2f2eaca1aa6bf6239e88bfd4d1ade844ecb
김수민 <liijj@naver.com>
2023-05-16 12:02:41 +0900

    [FE] refactor : bottom navbar 텃밭 삭제

--- client/lib/view/main/main_page.dart
+++ client/lib/view/main/main_page.dart
@@ -1,6 +1,5 @@
 import 'package:dokki/common/widget/bottom_navigation_bar_widget.dart';
 import 'package:dokki/view/book_search/search_book_page.dart';
-import 'package:dokki/view/dokki_grass/dokki_grass_page.dart';
 import 'package:dokki/view/home/home_page.dart';
 import 'package:dokki/view/library/library_page.dart';
 import 'package:dokki/view/profile/profile_page.dart';
@@ -19,11 +18,9 @@ class _MainPageState extends State<MainPage> {
   late String userId, nickname;
 
   List pages = [
-
     const HomePage(userId: ''),
     const SearchBookPage(userId: ''),
-    const LibraryPage(userId: ''),
-    const DokkiGrassPage(),
+    const LibraryPage(userId: '', nickname: ''),
     const ProfilePage(userId: ''),
   ];
 
@@ -44,11 +41,8 @@ class _MainPageState extends State<MainPage> {
 
         pages[0] = HomePage(userId: userId);
         pages[1] = SearchBookPage(userId: userId);
-        pages[2] = LibraryPage(
-          userId: userId,
-          nickname: nickname,
-        );
-        pages[4] = ProfilePage(userId: userId);
+        pages[2] = LibraryPage(userId: userId, nickname: nickname);
+        pages[3] = ProfilePage(userId: userId);
       });
     }
   }
