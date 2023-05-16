7c53f20e60fb648cca678a379a4557a67e676025
JCW <codnjs8221@naver.com>
2023-05-16 12:13:43 +0900

    [BE] feat : 이미지 의존성

--- backend/review/src/main/java/com/dokki/review/ReviewApplication.java
+++ backend/review/src/main/java/com/dokki/review/ReviewApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
@@ -18,6 +19,7 @@ import java.util.TimeZone;
 @EnableDiscoveryClient
 @EnableJpaAuditing // created, updated 관련
 @Slf4j
+@ComponentScan(basePackages = { "com.dokki.util" }) // FileUtils 빈 의존성
 public class ReviewApplication {
 
 	public static void main(String[] args) {
