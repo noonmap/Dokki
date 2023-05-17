ffa022862ba41677b2eef1ff313ed85c4323be7f
JCW <codnjs8221@naver.com>
2023-05-17 17:39:15 +0900

    [BE] feat : rest template 사용 시 timeout, retry, logging 설정 추가

--- backend/review/src/main/java/com/dokki/review/config/RestTemplateConfig.java
+++ backend/review/src/main/java/com/dokki/review/config/RestTemplateConfig.java
@@ -1,10 +1,22 @@
 package com.dokki.review.config;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.*;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.stream.Collectors;
 
 
 @Configuration
@@ -12,7 +24,60 @@ public class RestTemplateConfig {
 
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
+		return builder
+			.setConnectTimeout(Duration.ofSeconds(5)) // 5초 timeout 설정
+			.setReadTimeout(Duration.ofSeconds(5))
+			.additionalInterceptors(clientHttpRequestInterceptor(), new LoggingInterceptor()) // 실패 시 retry, 로깅
+			.requestFactory(() -> new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory())) // 응답 Stream의 데이터들이 컨슘된 상태라 데이터가 없어 에러가 발생하는 에러를 방지
+			.build();
+	}
+
+
+	public ClientHttpRequestInterceptor clientHttpRequestInterceptor() {
+		return (request, body, execution) -> {
+			RetryTemplate retryTemplate = new RetryTemplate();
+			retryTemplate.setRetryPolicy(new SimpleRetryPolicy(5)); // 5번까지 retry
+			try {
+				return retryTemplate.execute(context -> execution.execute(request, body));
+			} catch (Throwable throwable) {
+				throw new RuntimeException(throwable);
+			}
+		};
+	}
+
+
+	@Slf4j
+	static class LoggingInterceptor implements ClientHttpRequestInterceptor {
+
+		@Override
+		public ClientHttpResponse intercept(HttpRequest req, byte[] body, ClientHttpRequestExecution ex) throws IOException {
+			final String sessionNumber = makeSessionNumber();
+			printRequest(sessionNumber, req, body);
+			ClientHttpResponse response = ex.execute(req, body);
+			printResponse(sessionNumber, response);
+			return response;
+		}
+
+
+		private String makeSessionNumber() {
+			return Integer.toString((int) (Math.random() * 1000000));
+		}
+
+
+		private void printRequest(final String sessionNumber, final HttpRequest req, final byte[] body) {
+			log.info("[{}] URI: {}, Method: {}, Headers:{}, Body:{} ",
+				sessionNumber, req.getURI(), req.getMethod(), req.getHeaders(), new String(body, StandardCharsets.UTF_8));
+		}
+
+
+		private void printResponse(final String sessionNumber, final ClientHttpResponse res) throws IOException {
+			String body = new BufferedReader(new InputStreamReader(res.getBody(), StandardCharsets.UTF_8)).lines()
+				.collect(Collectors.joining("\n"));
+
+			log.info("[{}] Status: {}, Headers:{}, Body:{} ",
+				sessionNumber, res.getStatusCode(), res.getHeaders(), body);
+		}
+
 	}
 
 }
